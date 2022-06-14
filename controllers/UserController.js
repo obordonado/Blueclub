30cba8c863807afe0b89a57e8377040d876f7964
obordonado <obordonado@protonmail.com>
2022-06-14 19:42:17 +0200

    Creado UserController.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -9,7 +9,7 @@ let authConfig = require('../config/auth');
 const UserController = {};
 
 UserController.getUser = (req, res) => {
-    //Esta funcion llamada findAll es una funcion de Sequelize
+    //Funcion findAll de Sequelize
     User.findAll()
     .then(data => {
     
@@ -21,11 +21,15 @@ UserController.postUser = async (req, res) => {
 
     let name = req.body.name;
     let client_number = req.body.client_number;
+    let age = req.body.age;
+    let gender = req.body.gender;
     let password = bcryptjs.hashSync(req.body.password, Number.parseInt(authConfig.rounds));
 
     User.create({
         name: name,
         client_number: client_number,
+        age: age,
+        gender:gender,        
         password: password
     }).then(user => {
         res.send(`${user.name}, you have been added succesfully`);
@@ -50,15 +54,13 @@ UserController.loginUser = (req, res) => {
             res.send("Usuario o password incorrectos");
         } else {
             if( bcryptjs.compareSync(clave, usuarioEncontrado.password)){
-                //Ahora ya si hemos comprobado que el usuario existe (email es correcto) y el password SI corresponde a ese usuario
+                //Comprueba que el usuario existe y el password corresponde a ese usuario
 
                 let token = jwt.sign({ user: usuarioEncontrado }, authConfig.secret, {
                     expiresIn: authConfig.expires
                 });
-
-                console.log(token);
-                
-                let loginOKmessage = `Welcome again ${usuarioEncontrado.name}`
+                                
+                let loginOKmessage = `Bienvenido ${usuarioEncontrado.name}`
                 res.json({
                     loginOKmessage,
                     user: usuarioEncontrado,
