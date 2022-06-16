df9a95017b9ceca190dd3eaff8ea7694ea9f585a
obordonado <obordonado@protonmail.com>
2022-06-16 20:19:00 +0200

    isAdmin en rent ok postman

--- controllers/RentController.js
+++ controllers/RentController.js
@@ -5,29 +5,11 @@ const { Rent } = require('../models/index');
 const RentController = {};
 
 RentController.getAllRent = async (req, res) => {
-    
-    let id = req.params.id;
-
-    let consulta = `SELECT user.name AS UserName, film.name AS FilmName, rent.createdAt AS rent_date
-    
-    FROM user 
-    INNER JOIN Rent ON User.id = Rent.userId
-    INNER JOIN Film ON Film.id = Rent.FilmId
-    WHERE clienteId LIKE ${id};`;
-
-    //A continuación viene un ejemplo de ESTRUCTURA para ejecutar querys raw (crudo) de SQL en Sequelize, en este caso
-    //la consulta está almacenada dentro de la variable llamada consulta. Lo que nos devuelva la consulta se guardará en la variable
-    //resultado.
-
-    let resultado = await Rent.sequelize.query(consulta, {
-        type: Rent.sequelize.QueryTypes.SELECT
-    });
-
-    if(resultado != 0){
-        res.send(resultado);
-    }else {
-        res.send("Search error, please try again.");
-    };
+   Rent.findAll() 
+   .then(data=>{
+    res.send(data)
+   });
+   
 
 };
 
--- views/RentRouter.js
+++ views/RentRouter.js
@@ -1,12 +1,12 @@
 
 const express = require('express');
 const router = express.Router();
-
+const isAdmin= require("../middlewares/isAdmin");
 const RentController = require('../controllers/RentController');
 
 
 //Endpoint-function links
-router.get('/:id', RentController.getAllRent);
+router.get('/rent', isAdmin, RentController.getAllRent);
 
 
 
