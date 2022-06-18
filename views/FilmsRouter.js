a51e2dc3fd2bff13a8c5c867ad36d007107632cb
obordonado <obordonado@protonmail.com>
2022-06-18 09:26:44 +0200

    añadido comentarios

--- views/FilmsRouter.js
+++ views/FilmsRouter.js
@@ -9,8 +9,8 @@ const FilmsController = require('../controllers/FilmsController');
 //Endpoint-function links
 router.get('/', FilmsController.getFilms);
 router.post('/addfilm', FilmsController.postFilm);
-router.get('/search', FilmsController.searchFilm);
-router.get('/:id', FilmsController.getById);
+router.get('/search', FilmsController.searchFilm);///Find by movie name in body JSON
+router.get('/:id', FilmsController.getById);///Find movie by Id
 // router.get(`/delete`, FilmsController.delete);
 
 //Export
